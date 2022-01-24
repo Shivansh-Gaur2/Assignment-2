1628de7cdad4388b84131fc2193ee1275ad707d1
Shivansh-Gaur2 <96760872+Shivansh-Gaur2@users.noreply.github.com>
2022-01-24 17:05:16 +0530

    Update Question-3.py

--- Question-3.py
+++ Question-3.py
@@ -16,6 +16,8 @@ print("value of a after shifting left by 2 bits:- ",a)
 print("value of b after shifting left by 2 bits:- ",b)
 
 # E PART Right shift a with 2 bits and b with 4 bits.
+a = 56
+b = 10
 a = a>>2
 b = b>>4
 print("value of a after shifting right by 2 bits:- ",a)
